0ce9bfa63e6decebdec0675302cf257e57efc989
thenewpyjiang <pengyue3@illinois.edu>
2019-10-12 21:11:02 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Schulten Grid with Vue
+
+Schulte grid is back again! This time made with HTML/CSS/JavaScript + Vue.js + Electron.
+
+## Usage
+
+1. Change into the project directory with the `cd` command
+2. Run `npm install --save-dev electron`
+3. Run `npm install -g electron-packager` to install electron-packager
+4. Run `electron-packager ./ Schulte\ app` to pack the app according to platform
+5. Then find the program in your platform's format to play
+
---
+++ medium.js
@@ -0,0 +1,77 @@
+// Constants for game difficulty
+const EASY = 6
+const MEDIUM = 8
+const HARD = 9
+// Variable for game difficulty
+var difficulty = MEDIUM
+
+// Generate a random number series according to difficulty.
+function generateRandNumSeries(difficulty) {
+    var numSeries = []
+    for (var i = 0; i < difficulty * difficulty; i++) {
+        // Create a candidate integer
+        var temp = Math.ceil(Math.random() * difficulty * difficulty);
+        // Check for repetition
+        for (var j = 0; j <= i; j++) {
+            // If repeat found, try again
+            if (temp === numSeries[j]) {
+                i--
+                break
+            // Succeeds if no repetition
+            } else if (j == i) {
+                numSeries[i] = temp
+            }
+        }
+    }
+    return numSeries
+}
+
+// Turn difficulty into numerical representation
+function decodeDifficulty(difficulty) {
+    switch (difficulty) {
+        case "medium": return MEDIUM
+        case "hard": return HARD
+        default: return EASY
+    }
+}
+
+var vm = new Vue({
+    el: '#body',
+    data: {
+        difficulty: difficulty,
+        numSeries: generateRandNumSeries(difficulty)
+    }
+})
+
+// Variables
+// I forgot to initialize it, that's why it didn't work!
+var blocksDone = 0
+var tds = document.querySelectorAll('td')
+
+function addEventListenersForGrid() {
+    tds = document.querySelectorAll('td')
+    // Add event listeners for every block
+    for (var i = 0; i < tds.length; i++) {
+        tds[i].addEventListener('click', function() {
+            // If the player is clicking in the right order, text turns green
+            if (this.textContent == blocksDone + 1) {
+                if (blocksDone === 0) {
+                    startTime = new Date()
+                }
+                this.classList.add('done')
+                blocksDone++
+                if (blocksDone === tds.length) {
+                    endTime = new Date()
+                    var timeTaken = endTime - startTime
+                    document.querySelector('#grid').innerHTML =
+                        '<p id="message">' + 'You have finished in '
+                        + '<span id="highlight">'
+                        + Math.round(timeTaken / 1000)
+                        + '</span> seconds!</p>'
+                }
+            }
+        })
+    }
+}
+
+addEventListenersForGrid()
